dcd0e0d3cd5d87edd96a914b52d17407a0871855
linhusp <linhusp3@protonmail.com>
2026-07-18 12:05:07 +0700

    feat(nvim): custom strikethrough toggle in markdown

--- nvim/.config/nvim/after/ftplugin/markdown.lua
+++ nvim/.config/nvim/after/ftplugin/markdown.lua
@@ -1,13 +1,33 @@
 -- Attempt to add/remove strikethrough in markdown files
-G.map(
-    { 'v', 'x' },
-    '<leader>ms',
-    [[:s/^/\~\~/<CR>gv:s/$/\~\~/<CR>:noh<CR>]],
-    'Add strikethrough to a block in markdown'
-)
-G.map(
-    { 'v', 'x' },
-    '<leader>mS',
-    [[:s/^\~\~\(.*\)\~\~$/\1/<CR>:noh<CR>]],
-    'Remove strikethrough from a block in markdown'
-)
+vim.api.nvim_create_user_command('ToggleStrike', function(cmd)
+    local s = cmd.line1
+    local e = cmd.line2
+
+    local lines = vim.api.nvim_buf_get_lines(0, s - 1, e, false)
+    local strike_count = 0
+
+    -- Count lines wrapped in strikethrough
+    for _, line in ipairs(lines) do
+        if line:match('^%s*~~.*~~$') then strike_count = strike_count + 1 end
+    end
+
+    if strike_count == #lines then
+        -- If all lines have it then remove it and keep indentation
+        for i, line in ipairs(lines) do
+            local indent, content = line:match('^([%s]*)~~(.*)~~$')
+            if indent and content then lines[i] = indent .. content end
+        end
+    else
+        -- If mixed/none then add it to the missing lines and keep indentation
+        for i, line in ipairs(lines) do
+            local indent, content = line:match('^([%s]*)(.*)$')
+            if not line:match('^~~.*~~$') then
+                lines[i] = indent .. '~~' .. content .. '~~'
+            end
+        end
+    end
+
+    vim.api.nvim_buf_set_lines(0, s - 1, e, false, lines)
+end, { range = true })
+
+G.map({ 'n', 'x' }, '<leader>ms', ':ToggleStrike<CR>')
